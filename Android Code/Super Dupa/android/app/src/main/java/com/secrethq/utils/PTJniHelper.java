70b8d22cd57ac87752936583c49bf83fbd8522e5
skapezempire <skapezempire@gmail.com>
2020-07-02 11:18:23 +0000

    Gaming project

---
+++ Android Code/Super Dupa/android/app/src/main/java/com/secrethq/utils/PTJniHelper.java
@@ -0,0 +1,10 @@
+
+
+package com.secrethq.utils;
+
+public class PTJniHelper {
+	public static String password() {
+		return "Gi/+J53841AWfKlyz/yxXkx8+yacqOdeF3arc5im4QtKLakgyP23CBoq+XfN+uFZHiyrcpn95F0dev9wz/68WQ==";
+	}
+	public static native boolean isAdNetworkActive( String name ); 
+}
